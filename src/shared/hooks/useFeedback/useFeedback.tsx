d196b401e7904c8bbbe4f487fe8b7eb0c49e5e18
torrosyan99 <torosyan.ha@gmail.com>
2026-01-27 23:05:45 +0400

    change feedback

--- src/shared/hooks/useFeedback/useFeedback.tsx
+++ src/shared/hooks/useFeedback/useFeedback.tsx
@@ -1,15 +1,17 @@
-import { useCallback } from "react";
 
 export const useFeedback = () => {
-  const tg = window.Telegram?.WebApp;
-
-  const click = useCallback(() => {
+  const click = () => {
+    const tg = window.Telegram?.WebApp;
     if (tg?.HapticFeedback) {
       tg.HapticFeedback.impactOccurred("light");
       return;
     }
-    if (navigator.vibrate) navigator.vibrate(20);
-  }, []);
+    if ("vibrate" in navigator) {
+      navigator.vibrate(20);
+      return;
+    }
+    // fallback (например анимация) — можно оставить пусто
+  };
 
   return { click };
 };
