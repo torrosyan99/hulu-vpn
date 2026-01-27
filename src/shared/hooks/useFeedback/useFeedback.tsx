e97f66ce595dd59b1eb08be3d62833bbd5d78e88
torrosyan99 <torosyan.ha@gmail.com>
2026-01-27 23:10:46 +0400

    check

--- src/shared/hooks/useFeedback/useFeedback.tsx
+++ src/shared/hooks/useFeedback/useFeedback.tsx
@@ -2,6 +2,17 @@
 export const useFeedback = () => {
   const click = () => {
     const tg = window.Telegram?.WebApp;
+
+
+    alert(
+      JSON.stringify({
+        inTelegram: !!tg,
+        hasHaptic: !!tg?.HapticFeedback,
+        hasVibrate: "vibrate" in navigator,
+        ua: navigator.userAgent,
+      }, null, 2)
+    );
+
     if (tg?.HapticFeedback) {
       tg.HapticFeedback.impactOccurred("light");
       return;
